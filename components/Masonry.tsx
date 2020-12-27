93bfb1b4d5efec774d4cd19b81a23d104ee362b8
merlin04 <Merlin04@users.noreply.github.com>
2020-12-27 10:54:21 -0800

    Remove need for additional divs from Masonry

--- components/Masonry.tsx
+++ components/Masonry.tsx
@@ -3,7 +3,6 @@ import { CSSProperties } from "@material-ui/core/styles/withStyles";
 import { useMemo } from "react";
 
 interface StylesProps {
-    colWidth: string | number,
     calculatedProperties: CSSProperties
 }
 
@@ -16,18 +15,13 @@ const useStyles = makeStyles((theme) => ({
     })
 }));
 
-interface MasonryCalculations {
-    additionalStyles: CSSProperties,
-    additionalDivs: number
-}
-
 /**
  * Calculate styles for a given column width
  * @param cols Number of columns in the masonry
  * @param sizeMetric Array of item sizes
  * @returns CSS properties to be applied to the masonry container and the number of additional divs to add
  */
-function calculateMasonryForCols(cols: number, sizeMetrics: number[]): MasonryCalculations {
+function calculateMasonryForCols(cols: number, sizeMetrics: number[]): CSSProperties {
     // Get the order that the items should be in
 
     const rowCount = Math.ceil(sizeMetrics.length / cols);
@@ -44,9 +38,8 @@ function calculateMasonryForCols(cols: number, sizeMetrics: number[]): MasonryCa
         let fullRowMetrics = sizeMetrics
             // Get the items that should be in this row
             .slice(i * cols, (i + 1) * cols);
-        
-        const itemsInRowCount = fullRowMetrics.length;
-        
+                
+        // Add empty items to fill row
         for(let j = fullRowMetrics.length; j < cols; j++) {
             fullRowMetrics.push(0);
         }
@@ -74,28 +67,29 @@ function calculateMasonryForCols(cols: number, sizeMetrics: number[]): MasonryCa
             .map(item => item.originalIndex + i * cols));
     }
 
-    // Rotate the array to match CSS column order
+    // Rotate the array to match CSS column order, and replace non-existent indexes with undefined
 
-    let rotatedRows: number[] = [];
+    let rotatedRows: (number | undefined)[] = [];
     for(let i = 0; i < cols; i++) {
         for(let j = 0; j < rowCount; j++) {
-            rotatedRows.push(sortedRows[i + cols * j]);
+            let newVal: number | undefined = sortedRows[i + cols * j];
+            if(newVal >= sizeMetrics.length) {
+                newVal = undefined;
+            }
+            rotatedRows.push(newVal);
         }
     }
 
     // Generate CSS to set the order of the items
 
-    return {
-        additionalStyles: rotatedRows.length > 0 ? rotatedRows.map((item, index): CSSProperties => ({
-            [`& > *:nth-child(${item + 1})`]: {
-                order: index + 1,
-                pageBreakAfter: (index + 1) % rowCount === 0 ? "always" : "inherit"
-            }
-            // Flatten array of objects into a single object
-        })).reduce((acc, cur) => ({...acc, ...cur})) : {},
-        // Sometimes additional divs are needed to make the column layout work correctly
-        additionalDivs: (rotatedRows.length + Math.min(cols, sizeMetrics.length)) - sizeMetrics.length
-    };
+    return rotatedRows.length > 0 ? rotatedRows.map((item, index, array): CSSProperties => (item !== undefined ? {
+        [`& > *:nth-child(${item + 1})`]: {
+            order: index + 1,
+            // Break after the current item if it is the end of the column or if the next item is undefined
+            pageBreakAfter: ((index + 1) % rowCount === 0) || (array[index + 1] === undefined) ? "always" : "inherit"
+        }
+        // Flatten array of objects into a single object
+    } : {})).reduce((acc, cur) => ({...acc, ...cur})) : {};
 }
 
 interface MasonryProps {
@@ -112,31 +106,20 @@ interface MasonryProps {
 }
 
 export default function Masonry(props: MasonryProps) {
-    const masonryCalculations: MasonryCalculations = useMemo(() => {
-        let additionalDivs = 0;
+    const calculatedProperties: CSSProperties = useMemo(() => (
         // If there is only one column masonry doesn't need to be used
-        const additionalStyles = Array.from({length: props.maxCols - 1}, (_, i) => {
-            const masonry = calculateMasonryForCols(i + 2, props.sizeMetrics);
-            if(masonry.additionalDivs > additionalDivs) additionalDivs = masonry.additionalDivs;
-            return {
-                [`@media (min-width: calc(calc(${props.borderWidth} * 2) + calc(${props.colWidth} * ${i + 2})))`]: {
-                    ...masonry.additionalStyles
-                }
+        Array.from({length: props.maxCols - 1}, (_, i) => ({
+            [`@media (min-width: calc(calc(${props.borderWidth} * 2) + calc(${props.colWidth} * ${i + 2})))`]: {
+                ...calculateMasonryForCols(i + 2, props.sizeMetrics)
             }
-        }).reduce((acc, cur) => ({...acc, ...cur}));
+        })).reduce((acc, cur) => ({...acc, ...cur}))
+    ), []);
 
-        return {
-            additionalStyles,
-            additionalDivs
-        };
-    }, []);
-
-    const styles = useStyles({colWidth: props.colWidth, calculatedProperties: masonryCalculations.additionalStyles});
+    const styles = useStyles({calculatedProperties});
 
     return (
         <div className={styles.masonryContainer + (props.className !== undefined ? (" " + props.className) : "")}>
             {props.children}
-            {Array(masonryCalculations.additionalDivs).fill(<div></div>)}
         </div>
     )
 }
